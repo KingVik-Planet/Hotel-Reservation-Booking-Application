a4d890a99e64108a980988de98a925b56b5a681e
KingVik Planet <chukwukingsley56@gmail.com>
2024-07-05 15:53:14 +0200

    Ticket Confirmation

--- main.py
+++ main.py
@@ -1,35 +1,51 @@
 # Importing Modules
 import pandas as pd
 
+#Reading the File
+df = pd.read_csv("hotels.csv", dtype = {"id": str})
 
-#Reading the Hotel files
-df = pd.read_csv("hotels.csv")
 
-#Creating the Classes
 class Hotel:
-    def __init__(self, id):
-        pass
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
     def book(self):
-        pass
+        """Books the hotel and changes the availability to no"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index = False)
 
     def available(self):
-        pass
+        """Checks the availability of the hotel"""
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
 
 class TicketConfirmation:
     def __init__(self, customer_name,hotel_name):
-        pass
+        self.customer_name = customer_name
+        self.hotel = hotel_name
     def generate(self):
-        pass
+        content = f"""
+        Thank you for your booking our hotel!
+        Here are your booking details:
+        Name:{self.customer_name}
+        Hotel Name: {self.hotel.name}
+        """
+        return content
 
 
 print(df)
-id = input("Enter the id of the Hotel: ")
-hotel = Hotel(id)
+hotel_id = input("Enter the id of the hotel: ")
+hotel = Hotel(hotel_id)
+
 if hotel.available():
     hotel.book()
     name = input("Enter your name: ")
-    TicketConfirmation = TicketConfirmation(name, hotel)
+
+    TicketConfirmation = TicketConfirmation(customer_name=name, hotel_name=hotel)
     print(TicketConfirmation.generate())
 
 else:
-    print("Hotel is not currently available.")
+    print("Hotel is not available.")
