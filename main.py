0f9982a78b76bb664b1dda12a68a7d0edee5a4d8
KingVik Planet <chukwukingsley56@gmail.com>
2024-07-06 08:52:14 +0200

    Card and payment integration

--- main.py
+++ main.py
@@ -2,17 +2,19 @@
 import pandas as pd
 
 #Reading the File
-df = pd.read_csv("hotels.csv", dtype = {"id": str})
+df = pd.read_csv("hotels.csv", dtype={"id": str})
+df_cards = pd.read_csv("cards.csv", dtype=str).to_dict(orient="records")
 
 
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
         self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
+
     def book(self):
         """Books the hotel and changes the availability to no"""
         df.loc[df["id"] == self.hotel_id, "available"] = "no"
-        df.to_csv("hotels.csv", index = False)
+        df.to_csv("hotels.csv", index=False)
 
     def available(self):
         """Checks the availability of the hotel"""
@@ -22,10 +24,12 @@ class Hotel:
         else:
             return False
 
+
 class TicketConfirmation:
-    def __init__(self, customer_name,hotel_name):
+    def __init__(self, customer_name, hotel_name):
         self.customer_name = customer_name
         self.hotel = hotel_name
+
     def generate(self):
         content = f"""
         Thank you for your booking our hotel!
@@ -36,16 +40,33 @@ class TicketConfirmation:
         return content
 
 
+# Credit Card Class
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {"number": self.number, "expiration": expiration,
+                     "holder": holder, "cvc": cvc}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
+
 print(df)
 hotel_id = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_id)
 
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-
-    TicketConfirmation = TicketConfirmation(customer_name=name, hotel_name=hotel)
-    print(TicketConfirmation.generate())
+    credit_card = CreditCard(number="1234")
+    if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
+        hotel.book()
+        name = input("Enter your name: ")
+        TicketConfirmation = TicketConfirmation(customer_name=name, hotel_name=hotel)
+        print(TicketConfirmation.generate())
+    else:
+        print("There was a problem with your Payment")
 
 else:
     print("Hotel is not available.")
