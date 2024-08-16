aee7845d7d51df8a39c74091442360c756e2dfd7
KingVik Planet <chukwukingsley56@gmail.com>
2024-08-16 15:04:21 +0200

    Instant and property of class

--- main2.py
+++ main2.py
@@ -38,6 +38,16 @@ class TicketConfirmation:
         Hotel Name: {self.hotel.name}
         """
         return content
+
+
+    @property
+    def get_customer_name(self):
+        name = self.customer_name.strip()
+        name  = name.title()
+        return name
+
+
+
 hotel1 = Hotel(hotel_id="188")
 hotel2 = Hotel(hotel_id="134")
 
@@ -52,8 +62,25 @@ print(hotel2.watermark)
 
 print(Hotel.get_hotel_count(data =df))
 
-# Instant Method
-# Instant Variable
-# Class Variable
-#  Property  of Class
-# OOPs Concept
+
+
+ticket = TicketConfirmation(customer_name="john KELLY smith ", hotel_name= hotel1)
+print(ticket.get_customer_name)
+print(ticket.generate())
+
+
+
+
+############NOTE###########
+"""
+ Instant Method
+Instant Variable
+Class Variable
+
+
+() is used to call function
+- Property  of Class works as a variable, it uses @Property
+generate() -Method is inside the class and hidden until it is called
+generate_customer = Callable, it is variable - 
+
+"""
