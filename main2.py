42d1a4e9a9839aa045327148f82eb53c16b6dc31
KingVik Planet <chukwukingsley56@gmail.com>
2024-08-21 14:55:39 +0200

    Static Method, Magic Method

--- main2.py
+++ main2.py
@@ -21,9 +21,21 @@ class Hotel:
             return True
         else:
             return False
+
+# "@" is used for declaring the decorator
+
+#Class Method
     @classmethod
     def get_hotel_count(cls, data):
         return len(data)
+
+#Magic Method
+    def __eq__(self, other):
+        if self.hotel_id == other.hotel_id:
+            return True
+        else:
+            return False
+
 
 #Create class TicketConfirmation and define the functions
 class TicketConfirmation:
@@ -39,12 +51,19 @@ class TicketConfirmation:
         """
         return content
 
-
+#Property Method
     @property
     def get_customer_name(self):
         name = self.customer_name.strip()
         name  = name.title()
         return name
+
+#Static Method
+    @staticmethod
+    def convert(amount):
+        return amount * 1.2
+
+#Magic Method
 
 
 
@@ -63,23 +82,31 @@ print(hotel2.watermark)
 print(Hotel.get_hotel_count(data =df))
 
 
-
+#Property Method
 ticket = TicketConfirmation(customer_name="john KELLY smith ", hotel_name= hotel1)
 print(ticket.get_customer_name)
 print(ticket.generate())
 
+#Static Method
+converted =TicketConfirmation.convert(1000)
+print(converted)
 
-
-
-############NOTE###########
+############ NOTE ###########
 """
- Instant Method
-Instant Variable
+_Instant Method
+ 
+_Instant Variable
+
 Class Variable
 
+_ Static Method - Use to help instant method like utilities
+it used @staticmethod
+
+_ Magic Method (.__eq__):  for equating when 1 object has 2 ID
+like hotel 1 and hotel has room 188
 
 
-() is used to call function
+_ () is used to call function
 - Property  of Class works as a variable, it uses @Property
 generate() -Method is inside the class and hidden until it is called
 generate_customer = Callable, it is variable - 
